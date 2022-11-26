1ebfad5e0c7bd107ee20fbdf868e1aa685682b1d
Wilcascopr <97641210+Wilcascopr@users.noreply.github.com>
2022-11-26 12:48:09 -0500

    first commit and setup

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
+})
